b82ac1bf6e2a9f93c4738565911464d4f8fadde7
J0SH23I <ji7@geneseo.edu>
2023-12-17 22:36:11 -0500

    any message you want to have

---
+++ project.R
@@ -0,0 +1,12 @@
+git config --list
+git config --global user.email "ji7@geneseo.edu"
+git config --global user.name "J0SH23I"
+
+
+
+install.packages("usethis")
+usethis::create_github_token()
+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
+install.packages("gitcreds")
+gitcreds::gitcreds_set()
+
